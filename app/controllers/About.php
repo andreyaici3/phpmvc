6cbb97bb8ac93210c3635e3f8c81a04c2bc15947
andreyaici3 <andreyandri90@gmail.com>
2020-02-01 22:16:11 +0700

    menambahkan method view di controller

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,13 +1,21 @@
 <?php 
 
-class About {
+class About extends Controller {
 	public function index($nama = 'Andrey', $pekerjaan = "mahasiswa", $umur = 18)
 	{
-
-		echo "Halo Nama saya $nama, saya adalah seorang $pekerjaan. Saya berumur $umur tahun";
+		$data['nama'] = $nama;
+		$data['pekerjaan'] = $pekerjaan;
+		$data['umur'] = $umur;
+		$data['judul'] = 'Halaman About Me';
+		$this->view('templates/header',$data);
+		$this->view('about/index',$data);
+		$this->view('templates/footer');
 	}
 	public function page()
 	{
-		echo "about/page";
+		$data['judul'] = 'Halaman Pages';
+		$this->view('templates/header',$data);
+		$this->view('about/page');
+		$this->view('templates/footer');
 	}
 }
---
+++ app/views/about/index.php
@@ -0,0 +1,3 @@
+
+	<h1>About Me</h1>
+	<p>Halo Nama saya <?= $data['nama']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?>. Saya berumur <?= $data['umur']; ?> tahun</p>
