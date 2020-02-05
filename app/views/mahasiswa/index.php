77a43ea0cc6b6076383d6ae224daa204737c5e7c
andreyaici3 <andreyandri90@gmail.com>
2020-02-05 17:22:12 +0700

    menambahkan fitur update dengan json

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -7,15 +7,16 @@
 	<div class="row">
 		<div class="col-lg-6">
 			<!-- Button trigger modal -->
-			<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+			<button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModal">
 			  Tambah Data Mahasiswa
 			</button><br><br>
 			<h3>Daftar Mahasiswa</h3>				
 			<ul class="list-group">
 				<?php foreach ($data['mhs'] as $mhs): ?>
-					<li class="list-group-item "><?= $mhs['nama']; ?>
-						<a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary float-right ml-1">Detail</a>
+					<li class="list-group-item "><?= $mhs['nama']; ?>						
 						<a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge badge-danger ml-1 float-right" onClick="return confirm('Yakin ingin menghapus.?')">Hapus</a>
+						<a href="<?= BASEURL; ?>/mahasiswa/ubah/<?= $mhs['id']; ?>" class="badge badge-success float-right ml-1 tampilModalUbah" data-toggle="modal" data-target="#formModal" data-id="<?= $mhs['id']; ?>">Ubah</a>
+						<a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary float-right ml-1">Detail</a>
 				  	</li>
 				<?php endforeach ?>	  
 			</ul>
@@ -29,13 +30,14 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <h5 class="modal-title" id="formModalLabel">Tambah Data Mahasiswa</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
     <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="POST">
+    	<input type="hidden" name="id" id="id">
     	<div class="form-group">
 		    <label for="nama">Nama</label>
 		    <input type="text" class="form-control" id="nama" name="nama">
@@ -51,7 +53,7 @@
 		 <div class="form-group">
 		    <label for="jurusan">Jurusan</label>
 		    <select class="form-control" id="jurusan" name="jurusan">
-		      <option value="Teknik Informatik">Teknik Informatika</option>
+		      <option value="Teknik Informatika">Teknik Informatika</option>
 		      <option value="Teknik Industri">Teknik Industri</option>
 		      <option value="Teknik Mesin">Teknik Mesin</option>
 		      <option value="Teknik Planologi">Teknik Planologi</option>
